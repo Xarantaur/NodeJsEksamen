d380bda8f6972eb1aded6154c9cce99e3da954bd
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2024-05-24 14:26:45 +0200

    MessagesWorks

--- server/app.js
+++ server/app.js
@@ -45,6 +45,7 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
+    console.log("message sent from backend to frontend")
   });
 });
 
