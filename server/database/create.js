cbbe8f880d3e859915cf08637069a0a35a81d82d
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2024-05-26 13:30:05 +0200

    CreateMtgCardAPI

--- server/database/create.js
+++ server/database/create.js
@@ -1,10 +1,14 @@
 import db from "./connection.js"
 import findUser from "./read.js"
 
-const createUser = async (email, password, ) => {
+export const createUser = async (email, password, ) => {
     const newUser = await db.users.insertOne({ email: email, password: password})
     const user = await findUser(email)
     return user
 }
 
-export default createUser
+export const createCard = async (name, manacost, type, rarity, set, ability, power, toughness) => {
+    const newCard = await db.mtg.insertOne({ name: name, manacost: manacost, type: type, rarity: rarity, 
+                                             set: set, ability: ability, power: power, toughness: toughness});
+    return newCard;
+}
