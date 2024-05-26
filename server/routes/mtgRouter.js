d626e7fbc15c31dd21b9401f5c5b953d18b070a4
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2024-05-26 14:25:52 +0200

    started on get router

--- server/routes/mtgRouter.js
+++ server/routes/mtgRouter.js
@@ -1,18 +1,26 @@
+import { findCard } from "../database/read";
 
 
 router.get("/api/mtg", (req, res) => {
-    
+    const { name } = req.body;
+    const card = findCard(name)
+    if(!card) {
+        console.log("no card found")
+    }else{
+        toast.succes("")
+    }
   res.send({ data: "stuff" });
 });
+
 
 router.post("/api/mtg", (req, res) => {
   res.send({ data: "stuff" });
 });
 
-router.patch("/api/mtg/", (req, res) => {
+router.patch("/api/mtg", (req, res) => {
   res.send({ data: "stuff" });
 });
 
-router.delete("/api/mtg/", (req, res) => {
+router.delete("/api/mtg", (req, res) => {
   res.send({ data: "stuff" });
 });
